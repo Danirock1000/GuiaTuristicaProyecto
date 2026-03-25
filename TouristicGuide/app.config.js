bbadd31d0da99e9e2fe4d23b361b11e986746338
AnthonioFuentes09 <AntonioFuentes.09.01@gmail.com>
2026-03-25 01:41:46 -0600

    2026-03-25 | app.config + capa de seguridad por api key google
    Se añadio el archivo de app.config.js, debido a que se desea mantener la api key de google segura, ya se actualizo la api key a otra nueva, la cual sera compartida con el equipo.

---
+++ TouristicGuide/app.config.js
@@ -0,0 +1,35 @@
+import "dotenv/config";
+
+export default {
+  expo: {
+    name: "TouristicGuide",
+    slug: "TouristicGuide",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    splash: {
+      image: "./assets/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      adaptiveIcon: {
+        backgroundColor: "#E6F4FE",
+        foregroundImage: "./assets/android-icon-foreground.png",
+        backgroundImage: "./assets/android-icon-background.png",
+        monochromeImage: "./assets/android-icon-monochrome.png",
+      },
+      predictiveBackGestureEnabled: false,
+    },
+    web: {
+      favicon: "./assets/favicon.png",
+    },
+    extra: {
+      googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
+    },
+  },
+};
